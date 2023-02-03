911bea550273639887dc206a39f1a8a65d255ba2
Felipe <tomazalvesfelipe@gmail.com>
2023-02-03 15:21:24 -0400

    Feat: Listagem produtos

---
+++ components/ProductItem.js
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link legacyBehavior href={`/produto/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link legacyBehavior href={`/produto/${product.slug}`}>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>R${product.price}</p>
+        <button className="primary-button" type="button">
+          Adicionar ao carrinho
+        </button>
+      </div>
+    </div>
+  );
+}
